dcab7466daad6e8332c36e141adf8c8c147bafd6
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-04-27 10:03:27 -0300

    Desenvolvimento do CRUD com Flask - Read - parte 2

--- app.py
+++ app.py
@@ -6,25 +6,35 @@ app = Flask(__name__)
 tasks = []
 tasksIdControl = 1
 
+
 @app.route("/tasks", methods=["POST"])
 def create_tasks():
     global tasksIdControl
     data = request.get_json()
-    new_task = Task(id=tasksIdControl, title=data.get("title"), description=data.get("description", " "))
+    new_task = Task(
+        id=tasksIdControl,
+        title=data.get("title"),
+        description=data.get("description", " "),
+    )
     tasksIdControl += 1
     tasks.append(new_task)
-    return  jsonify({"message":"Nova tarefa criada com sucesso"})
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
 
 
 @app.route("/tasks", methods=["GET"])
 def read_tasks():
-    taskList = [task.to_dict()  for task in tasks] 
-    output={
-        "tasks":taskList,
-        "total_tasks":0
-    }
+    taskList = [task.to_dict() for task in tasks]
+    output = {"tasks": taskList, "total_tasks": len(taskList)}
 
     return jsonify(output)
+
+@app.route("/tasks/<int:id>", methods=["GET"])
+def read_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message":"id nao encontrado"}), 404    
 
 
 if __name__ == "__main__":
